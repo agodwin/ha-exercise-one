a755c64d254dce0eec53ded8dbeab119d6af72d6
Tony Godwin <ajgodwin@gmail.com>
2016-02-04 12:53:56 -0600

    fell better looking at the Checkout progress bar

--- page/Checkout_Cart.rb
+++ page/Checkout_Cart.rb
@@ -4,22 +4,27 @@ class Checkout_Cart
   
   def valid?
       browser.title().start_with?("Checkout") &&
-          browser.li(:class => 'cart act')
-
+          progress_bar_element('cart').exists?
   end
   
   def loaded?
       browser.table(:class => 'checkout_cart').wait_until_present
+      progress_bar_element('cart').wait_until_present
       self.valid?
   end
   
   def continue
       continue
-      next_page = Black_iPhone_4s.new(browser)
+      next_page = Checkout_Info.new(browser)
       next_page.loaded?        
   end
   
 private
     button(:remove_item, :value => 'Remove', :name => 'submit')
     link(:continue, :class => 'step2')
+
+    #TODO: move this into a page/Page_Helper.rb as it is used in other Checkout pages
+    def progress_bar_element(stage)
+       browser.div(:class => 'progress_wrapper', :class => 'top').ul.li(:class => stage, :class => 'act')
+    end
 end
--- page/Checkout_Info.rb
+++ page/Checkout_Info.rb
@@ -4,21 +4,20 @@ class Checkout_Info
   
   def valid?
       browser.title().start_with?("Checkout") &&
-          browser.li(:class => 'info act')
+         progress_bar_element('info').exists?
   end
   
   def loaded?
       browser.div(:id => 'wpsc_shopping_cart_container').wait_until_present
+      progress_bar_element('info').wait_until_present
+
       self.valid?
   end
   
-  def continue
-      continue
-      next_page = Black_iPhone_4s.new(browser)
-      next_page.loaded?        
-  end
-  
 private
-    button(:remove_item, :value => 'Remove', :name => 'submit')
-    link(:continue, :class => 'step2')
+
+    #TODO: move this into a page/Page_Helper.rb as it is used in other Checkout pages
+    def progress_bar_element(stage)
+       browser.div(:class => 'progress_wrapper', :class => 'top').ul.li(:class => stage, :class => 'act')
+    end
 end
